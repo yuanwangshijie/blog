8fb94d974c5c2340b4162213c1ec99786934510e
WangQiang <qiang.wang@mirrortech.com>
2022-12-26 10:17:15 +0800

    update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,6 +4,7 @@ import theme from "./theme.js";
 export default defineUserConfig({
     lang: "zh-CN",
     title: "远望世界' Blog",
+    description: "作者自用的博客，用于学习记录",
     theme,
     shouldPrefetch: false,
 });
