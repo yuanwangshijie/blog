336a82749e7b72b005f8eca2ed7ec8a00e15526a
yuanwangshijie <yuanwangshijie@gmail.com>
2022-12-26 21:46:37 +0800

    update docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,18 +8,18 @@ export default defineUserConfig({
     theme,
     shouldPrefetch: false,
     plugins: [
-        // 搜索插件，开销较大
+        // 客户端搜索插件，开销较大
         searchProPlugin({
             // 索引全部内容
             indexContent: true,
             // 为分类和标签添加索引
             customFields: [
                 {
-                    getter: (page) => page.frontmatter.category,
+                    getter: (page) => page.frontmatter.category as string,
                     formatter: "分类：$content",
                 },
                 {
-                    getter: (page) => page.frontmatter.tag,
+                    getter: (page) => page.frontmatter.tag as string,
                     formatter: "标签：$content",
                 },
             ],
