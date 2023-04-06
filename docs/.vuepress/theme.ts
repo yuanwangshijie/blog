9d72f775ed485a667b9ecd181c556646b194ac5e
yuanwangshijie <yuanwangshijie@gmail.com>
2023-04-07 01:00:25 +0800

    更新blog

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -62,6 +62,7 @@ export default hopeTheme({
             repoId: "R_kgDOJTjjZA",
             category: "Announcements",
             categoryId: "DIC_kwDOJTjjZM4CVl69",
+            mapping: "url",
 
             /**
              * Using Twikoo
